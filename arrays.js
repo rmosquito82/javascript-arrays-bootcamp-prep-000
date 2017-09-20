25e5eb8f5d0d28dbd7bc69df04fd3094b318a0b4
r_lito <rui.mosquito@gmail.com>
2017-09-20 19:31:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -48,3 +48,8 @@ function destructivelyAddElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
+
+function accessElementInArray(array, index){
+  console.log(array[index])
+}
+
