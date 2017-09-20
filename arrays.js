e76cca174893985ba2eb76d3cd0a866d7e7cf6af
r_lito <rui.mosquito@gmail.com>
2017-09-20 19:58:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -59,8 +59,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
-  return array
+  newArray = array.slice(1)
+  return newArray
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
